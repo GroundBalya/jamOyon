b5208abc8cd6f4d0c61a876f319ffc0ea77b9584
GroundBalya <balyaground03@gmail.com>
2024-06-15 18:35:19 +0700

    haloo

--- frontend/src/component/addItemForm.js
+++ frontend/src/component/addItemForm.js
@@ -10,7 +10,7 @@ const AddItemForm = ({ handleAdd }) => {
         price: '',
         location: ''
     });
-
+// tes comment 
     const handleChange = (e) => {
         const { name, value } = e.target;
         setFormData((prevData) => ({
